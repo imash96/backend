69ab7b3500e7c5951d6e00a53f2d563d55c506a9
imash96 <arshad.ans7006@gmail.com>
2023-01-10 12:41:10 +0530

    Update medusa-config.js

--- medusa-config.js
+++ medusa-config.js
@@ -26,7 +26,7 @@ try {
 const ADMIN_CORS = process.env.ADMIN_CORS || "https://admin.leatherlifestyle.store";
 
 // CORS to avoid issues when consuming Medusa from a client
-const STORE_CORS = process.env.STORE_CORS || "https://leatherlifestyle.store, https://www.leatherlifestyle.store";
+const STORE_CORS = process.env.STORE_CORS || "https://leatherlifestyle.store, https://www.leatherlifestyle.store, http://localhost:8000, localhost:8000";
 
 // Database URL (here we use a local database called medusa-development)
 const DATABASE_URL =
